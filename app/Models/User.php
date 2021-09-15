b42fdedb88fc68bc4cace0e6d583f749e31f8719
payald1992 <payal.desai@retailogy.com>
2021-09-15 16:31:36 +0530

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
 
     public $timestamps = true;
 
